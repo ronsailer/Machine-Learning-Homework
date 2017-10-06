11d4b6ccde150a851bc2a06afd9729ad6d55dd8c
Ron Sailer <ronsailer@gmail.com>
2017-10-06 16:39:05 +0300

    Added hw3, first steps of machine learning

---
+++ hw3/run_jupyter_hw03_data_preparation.cmd
@@ -0,0 +1,3 @@
+cd "C:\Python27\Scripts\"
+
+jupyter2 notebook "c:\Users\Adam\Desktop\Technion\Sailerdreich.Git\sailerdreich\mlhw\hw3\HW03_Data_Preparation.ipynb" --NotebookApp.iopub_data_rate_limit="1000000000"
